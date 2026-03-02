dc7b6a2189e23e67ab5861996369d2bb11f5aafd
Anna Gritsenko <angritsen@gmail.com>
2026-03-02 17:43:46 +0300

    homework-3

--- dist/common/constants.js
+++
@@ -1,7 +0,0 @@
-export const STATUS_OK = 200;
-export const STATUS_CREATED = 201;
-export const STATUS_NOT_FOUND = 404;
-export const STATUS_CONFLICT = 409;
-export const STATUS_SERVER_ERROR = 500;
-export const STATUS_UNAUTHORIZED = 401;
-export const STATUS_FORBIDDEN = 403;
